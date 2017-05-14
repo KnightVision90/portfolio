9b161b2d17549a1c9e2e3b70a3d962ecbc7c3a9a
Joseph Moran <joseph.moraniii@gmail.com>
2017-05-14 19:05:30 -0400

    Add footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="footer" class="text-xs-center">
+    <div class="made-by">
+      Made With Love By Me
+    </div>
+    <div class="copywrite">
+      &copy;{{ year }}
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'footer',
+    data() {
+      return {
+        year: (new Date()).getFullYear(),
+      };
+    },
+  };
+</script>
+
+<style lang="scss" scoped>
+  @import '../assets/css/_colors';
+
+  #footer {
+    padding: 2vh 0;
+    background-color: $black;
+    font-weight: bold;
+
+    .made-by {
+      color: $white;
+    }
+
+    .copywrite {
+      color: $light_blue;
+    }
+  }
+</style>
--- src/components/Portfolio.vue
+++ src/components/Portfolio.vue
@@ -4,12 +4,14 @@
     <about-component></about-component>
     <projects-component></projects-component>
     <contact-component></contact-component>
+    <footer-component></footer-component>
   </div>
 </template>
 
 <script>
 import AboutComponent from './About';
 import ContactComponent from './Contact';
+import FooterComponent from './Footer';
 import OverviewComponent from './Overview';
 import ProjectsComponent from './Projects';
 
@@ -18,6 +20,7 @@ export default {
   components: {
     AboutComponent,
     ContactComponent,
+    FooterComponent,
     OverviewComponent,
     ProjectsComponent,
   },
